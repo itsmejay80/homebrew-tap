2a231d85a819a6a304d6c23a2da707c3936ebe9d
Jayesh Patel <jaypatel6963@gmail.com>
2026-04-24 21:07:47 -0400

    fix: use on_arm/on_intel in nook cask

--- Casks/nook.rb
+++ Casks/nook.rb
@@ -1,15 +1,19 @@
 cask "nook" do
   version "0.1.7"
 
-  arch arm: "arm64", intel: "x64"
+  on_arm do
+    sha256 "b2efb74fc3631fa5899ef07f736fcf79b38380888dd6d6191fa4db76425af542"
+    url "https://github.com/itsmejay80/nook/releases/download/v#{version}/Nook-#{version}-arm64.dmg"
+  end
 
-  url "https://github.com/itsmejay80/nook/releases/download/v#{version}/Nook-#{version}-#{arch}.dmg"
+  on_intel do
+    sha256 "19b05fc3d1291fcd8cb345ce241af9d0b83894b4d94fb7ccdc29e4555b31dd1e"
+    url "https://github.com/itsmejay80/nook/releases/download/v#{version}/Nook-#{version}-x64.dmg"
+  end
+
   name "Nook"
   desc "Local-first, spatial workboard desktop app"
   homepage "https://github.com/itsmejay80/nook"
-
-  sha256 arm:   "b2efb74fc3631fa5899ef07f736fcf79b38380888dd6d6191fa4db76425af542",
-         intel: "19b05fc3d1291fcd8cb345ce241af9d0b83894b4d94fb7ccdc29e4555b31dd1e"
 
   app "Nook.app"
 
